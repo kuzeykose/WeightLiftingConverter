4ef7a8c4d3948e72214e29a2acb1da8b708d1510
Kuzey Kose <kosekuzey97@gmail.com>
2024-05-10 10:38:09 -0400

    Initial Commit

---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by Kuzey Köse on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
